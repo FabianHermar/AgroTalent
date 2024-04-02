3c0f746d520d05f262ec45fe8cda8311fb2301c7
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-04-02 02:50:19 -0600

    Add new font families and colors to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,27 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        GilroyRegular: 'GilroyRegular, system-ui, sans-serif',
+        GilroyMedium: 'GilroyMedium, system-ui, sans-serif',
+        GilroySemibold: 'GilroySemibold, system-ui, sans-serif',
+        GilroyBold: 'GilroyBold, system-ui, sans-serif'
+      },
+      colors: {
+        'green-pea': {
+          '50': '#f1f8f3',
+          '100': '#ddeee0',
+          '200': '#bdddc5',
+          '300': '#91c4a1',
+          '400': '#62a579',
+          '500': '#41885b',
+          '600': '#2f6c47',
+          '700': '#2a6041',
+          '800': '#204530',
+          '900': '#1b3928',
+          '950': '#0e2017',
+        },
+      }
     },
   },
   plugins: [],
