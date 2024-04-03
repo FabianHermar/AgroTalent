8e719f5b7a315fa48b8ad452da4d78b610966a02
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-04-03 00:06:46 -0600

    Refactor tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,41 +1,36 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      fontFamily: {
-        GilroyRegular: 'GilroyRegular, system-ui, sans-serif',
-        GilroyMedium: 'GilroyMedium, system-ui, sans-serif',
-        GilroySemibold: 'GilroySemibold, system-ui, sans-serif',
-        GilroyBold: 'GilroyBold, system-ui, sans-serif'
-      },
-      colors: {
-        'green-pea': {
-          '50': '#f1f8f3',
-          '100': '#ddeee0',
-          '200': '#bdddc5',
-          '300': '#91c4a1',
-          '400': '#62a579',
-          '500': '#41885b',
-          '600': '#2f6c47',
-          '700': '#2a6041',
-          '800': '#204530',
-          '900': '#1b3928',
-          '950': '#0e2017',
-        },
-      }
-    },
-  },
-  plugins: [],
-};
-export default config;
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}'
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				GilroyRegular: 'GilroyRegular, system-ui, sans-serif',
+				GilroyMedium: 'GilroyMedium, system-ui, sans-serif',
+				GilroySemibold: 'GilroySemibold, system-ui, sans-serif',
+				GilroyBold: 'GilroyBold, system-ui, sans-serif'
+			},
+			colors: {
+				'green-pea': {
+					'50': '#f1f8f3',
+					'100': '#ddeee0',
+					'200': '#bdddc5',
+					'300': '#91c4a1',
+					'400': '#62a579',
+					'500': '#41885b',
+					'600': '#2f6c47',
+					'700': '#2a6041',
+					'800': '#204530',
+					'900': '#1b3928',
+					'950': '#0e2017'
+				}
+			}
+		}
+	},
+	plugins: []
+}
+export default config
