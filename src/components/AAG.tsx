c25989f49004214adc36a840e1b0224178981771
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-04-03 00:09:11 -0600

    Add AAG component with agronegocios, agricultura, and ganadería sections

---
+++ src/components/AAG.tsx
@@ -0,0 +1,52 @@
+export default function AAG() {
+	return (
+		<section>
+			<div className='pt-[120px] pb-[120px] grid md:grid-cols-1 grid-rows-1'>
+				<div className='flex flex-col md:flex-row items-center lg:justify-center'>
+					<article className='flex items-center flex-col justify-center'>
+						<img
+							src='/svg/agronegocios.svg'
+							alt=''
+							className='w-[120px] drop-shadow-lg select-none'
+						/>
+						<h3 className='font-GilroyBold text-3xl py-4 text-center'>
+							Agronegocios
+						</h3>
+						<p className='font-GilroyMedium text-center text-base pt-2 px-5 lg:px-10 '>
+							Actividades comerciales y financieras relacionadas con la
+							producción agrícola y ganadera.
+						</p>
+					</article>
+					<article className='flex items-center flex-col justify-center'>
+						<img
+							src='/svg/agricultura.svg'
+							alt=''
+							className='w-[120px] drop-shadow-lg select-none pt-12 md:pt-0'
+						/>
+						<h3 className='font-GilroyBold text-3xl py-4 text-center'>
+							Agricultura
+						</h3>
+						<p className='font-GilroyMedium text-center text-base pt-2 px-5 lg:px-10'>
+							Siembra y el cuidado de los cultivos hasta la cosecha y la
+							comercialización de los productos.
+						</p>
+					</article>
+					<article className='flex items-center flex-col justify-center'>
+						<img
+							src='/svg/ganaderia.svg'
+							alt=''
+							className='w-[120px] drop-shadow-lg select-none pt-12 md:pt-0'
+						/>
+						<h3 className='font-GilroyBold text-3xl py-4 text-center'>
+							Ganadería
+						</h3>
+						<p className='font-GilroyMedium text-center text-base pt-2 px-5 lg:px-10'>
+							Crianza y el cuidado de animales para la producción de carne,
+							leche, huevos, cuero y otros productos derivados.
+						</p>
+					</article>
+				</div>
+			</div>
+		</section>
+	)
+}
