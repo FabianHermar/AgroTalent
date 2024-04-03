95d7ec3116bd0901da20fef41589086e5bc4e710
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-04-03 00:08:43 -0600

    Add AAG component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
+import AAG from '@/components/AAG'
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
 
 export default function Home() {
-  return (
-    <main>
-      <Header />
-      <Hero />
-    </main>
-  );
+	return (
+		<main>
+			<Header />
+			<Hero />
+			<AAG />
+		</main>
+	)
 }
