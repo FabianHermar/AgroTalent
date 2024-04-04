690af3eaacc6ece47bd48af0cd421f822006a2d7
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2024-04-04 17:49:11 -0600

    Refactor PopularOffers component to use new images and styles

--- src/components/PopularOffers.tsx
+++ src/components/PopularOffers.tsx
@@ -14,61 +14,88 @@ export default function PopularOffers() {
 			</div>
 			<div className={styles.POparent}>
 				<article className={styles.POBento1}>
-					<div className='px-6 py-6'>
-						<a
-							href='#'
-							className='size-9 rounded-full bg-green-pea-400 absolute right-0 mr-6 flex justify-center items-center'
-						>
-							<img
-								src='/svg/heart.svg'
-								alt='Heart icon'
-								className='size-5 hover:scale-105 transition-transform duration-75 select-none'
-							/>
-						</a>
-					</div>
+          <img
+            alt='Agroculture image'
+            src='https://images.unsplash.com/photo-1625246333195-78d9c38ad449?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            className='absolute inset-0 h-full w-full object-cover select-none'
+          />
+          <div className='relative bg-gradient-to-t from-gray-900/40 to-gray-900/25 w-full h-full'>
+            <div className='px-6 py-6'>
+              <a
+                href='#'
+                className='size-9 rounded-full bg-white absolute right-0 mr-6 flex justify-center items-center'
+              >
+                <img
+                  src='/svg/heart.svg'
+                  alt='Heart icon'
+                  className='size-5 hover:scale-105 transition-transform duration-75 select-none'
+                />
+              </a>
+            </div>
+          </div>
 				</article>
 				<article className={styles.POBento2}>
-					<div className='px-6 py-6'>
-						<a
-							href='#'
-							className='size-9 rounded-full bg-green-pea-400 absolute right-0 mr-6 flex justify-center items-center'
-						>
-							<img
-								src='/svg/heart.svg'
-								alt='Heart icon'
-								className='size-5 hover:scale-105 transition-transform duration-75 select-none'
-							/>
-						</a>
-					</div>
+          <img
+            alt='Agroculture image'
+            src='https://images.unsplash.com/photo-1500595046743-cd271d694d30?q=80&w=3874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            className='absolute inset-0 h-full w-full object-cover select-none'
+          />
+          <div className='relative bg-gradient-to-t from-gray-900/40 to-gray-900/25 h-full w-full'>
+            <div className='px-6 py-6'>
+              <a
+                href='#'
+                className='size-9 rounded-full bg-white absolute right-0 mr-6 flex justify-center items-center'
+              >
+                <img
+                  src='/svg/heart.svg'
+                  alt='Heart icon'
+                  className='size-5 hover:scale-105 transition-transform duration-75 select-none'
+                />
+              </a>
+            </div>
+          </div>
 				</article>
 				<article className={styles.POBento3}>
-					<div className='px-6 py-6'>
-						<a
-							href='#'
-							className='size-9 rounded-full bg-green-pea-400 absolute right-0 mr-6 flex justify-center items-center'
-						>
-							<img
-								src='/svg/heart.svg'
-								alt='Heart icon'
-								className='size-5 hover:scale-105 transition-transform duration-75 select-none'
-							/>
-						</a>
+          <img
+            alt='Agroculture image'
+            src='https://images.unsplash.com/photo-1532509774891-141d37f25ae9?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            className='absolute inset-0 h-full w-full object-cover select-none'
+          />
+          <div className='relative bg-gradient-to-t from-gray-900/40 to-gray-900/25 w-full h-full'>
+            <div className='px-6 py-6'>
+              <a
+                href='#'
+                className='size-9 rounded-full bg-white absolute right-0 mr-6 flex justify-center items-center'
+              >
+                <img
+                  src='/svg/heart.svg'
+                  alt='Heart icon'
+                  className='size-5 hover:scale-105 transition-transform duration-75 select-none'
+                />
+              </a>
+            </div>
 					</div>
 				</article>
 				<article className={styles.POBento4}>
-					<div className='px-6 py-6'>
-						<a
-							href='#'
-							className='size-9 rounded-full bg-green-pea-400 absolute right-0 mr-6 flex justify-center items-center'
-						>
-							<img
-								src='/svg/heart.svg'
-								alt='Heart icon'
-								className='size-5 hover:scale-105 transition-transform duration-75 select-none'
-							/>
-						</a>
-					</div>
-					48
+          <img
+            alt='Agroculture image'
+            src='https://images.unsplash.com/photo-1624720114708-0cbd6ee41f4e?q=80&w=3869&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            className='absolute inset-0 h-full w-full object-cover select-none'
+          />
+          <div className='relative bg-gradient-to-t from-gray-900/40 to-gray-900/25 w-full h-full'>
+            <div className='px-6 py-6'>
+              <a
+                href='#'
+                className='size-9 rounded-full bg-white absolute right-0 mr-6 flex justify-center items-center'
+              >
+                <img
+                  src='/svg/heart.svg'
+                  alt='Heart icon'
+                  className='size-5 hover:scale-105 transition-transform duration-75 select-none'
+                />
+              </a>
+            </div>
+          </div>
 				</article>
 			</div>
 		</section>
